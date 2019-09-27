414ed2a24923cbfb4ff9034cb12049bcd5c8fedf
Qing Wang <chingwang.qw@gmail.com>
2019-09-27 19:30:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,8 +100,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     
-     programmer_hash[:alan_kay] [:known_for] = [:alans_new_info]["GUI"]
+    return  programmer_hash[:alan_kay] [:known_for] = [:alans_new_info]["GUI"]
+  end 
      
      return programmer_hash
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.  alans_new_info = "GUI"
