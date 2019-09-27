49a5258e4640c6dedd144fb4566436a846c7da22
Qing Wang <chingwang.qw@gmail.com>
2019-09-27 19:18:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,26 +1,24 @@
 
 def hopper()
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-     }
+	programmer_hash = {
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+   }
      return programmer_hash
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = {
       :grace_hopper => {
         :known_for => "COBOL",
@@ -36,7 +34,6 @@ def alan_kay_is_known_for
       }
    }
   return  programmer_hash[:alan_kay][:known_for]
-  
 end
 
 def dennis_ritchies_language
