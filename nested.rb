c5123a2f50fab15f32531066f40f9e0209afc12f
Qing Wang <chingwang.qw@gmail.com>
2019-09-27 19:16:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,5 +1,5 @@
 
-def hopper
+def hopper()
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -79,7 +79,7 @@ def adding_matz
         }
      }
 
-  programmer_hash = [:yukihiro_matsumoto => { :known_for => "Ruby", :languages => ["LISP", "C"]] 
+  programmer_hash[:yukihiro_matsumoto] = [:known_for => "Ruby", :languages => ["LISP", "C"]] 
     
   return programmer_hash
   
@@ -105,17 +105,15 @@ def changing_alan
      programmer_hash[:alan_kay] [:known_for] = [:alans_new_info]["GUI"]
      
      return programmer_hash
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.  alans_new_info = "GUI"
 end
 
 
 def adding_to_dennis
 	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+ 		{  :grace_hopper => {
+       :known_for => "COBOL",
+       :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
           :known_for => "Object Orientation",
